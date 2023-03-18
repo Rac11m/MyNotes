5bcee002981753cae0be6e8b433ed645dad74571
Racim Saal <racimsaal@gmail.com>
2023-03-18 14:19:59 +0100

    step-19

--- test/auth_test.dart
+++ test/auth_test.dart
@@ -108,7 +108,11 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializedException();
     if (email == "foo@bar.com") throw UserNotFoundAuthException();
     if (password == "foobar") throw WrongPasswordAuthException();
-    const user = AuthUser(isEmailVerified: false, email: 'foo@bar.baz');
+    const user = AuthUser(
+      id: 'my_id',
+      isEmailVerified: false,
+      email: 'foo@bar.baz',
+    );
     _user = user;
     return Future.value(user);
   }
@@ -126,7 +130,11 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializedException();
     final user = _user;
     if (user == null) throw UserNotFoundAuthException();
-    const newUser = AuthUser(isEmailVerified: true, email: 'foo@bar.baz');
+    const newUser = AuthUser(
+      id: 'my_id',
+      isEmailVerified: true,
+      email: 'foo@bar.baz',
+    );
     _user = newUser;
   }
 }
